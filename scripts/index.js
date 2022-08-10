a0c22003beca49aa0808cea80982eec9057b416a
Irina Alyabeva <irinpingviiin@yandex.ru>
2022-08-10 12:50:34 +0400

    Исправления по замечаниям ревьювера

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
-let popup = document.querySelector('.popup');
-let popupForm = popup.querySelector('.popup__form');
-let popupNameField = popup.querySelector('.popup__name-field');
-let popupDescriptionField = popup.querySelector('.popup__description-field');
-let popupCloseButton = popup.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const popupForm = popup.querySelector('.popup__form');
+const popupNameField = popup.querySelector('.popup__input_text_name');
+const popupDescriptionField = popup.querySelector('.popup__input_text_description');
+const popupCloseButton = popup.querySelector('.popup__close-button');
 
-let profile = document.querySelector('.profile');
-let profileEditButton = profile.querySelector('.profile__edit-button');
-let profileName = profile.querySelector('.profile__name');
-let profileDescription = profile.querySelector('.profile__description');
+const profile = document.querySelector('.profile');
+const profileEditButton = profile.querySelector('.profile__edit-button');
+const profileName = profile.querySelector('.profile__name');
+const profileDescription = profile.querySelector('.profile__description');
 
 function editProfileInfo() {
   popup.classList.add('popup_opened');
@@ -23,7 +23,7 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = popupNameField.value;
   profileDescription.textContent = popupDescriptionField.value;
-  popup.classList.remove('popup_opened');
+  closeEditForm();
 }
 
 profileEditButton.addEventListener('click', editProfileInfo);
